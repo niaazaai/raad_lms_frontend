240e7d77128be911013a2892ba5ff63ba847fe85
Maseeh Niazaai <maseeh.niazaai@gmail.com>
2026-04-29 17:49:15 +0430

    Update MainLayout styles for improved spacing and layout consistency
    - Adjusted padding in the main section of MainLayout to enhance visual hierarchy and user experience on larger screens.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -19,7 +19,7 @@ const MainLayout = () => {
       >
         <Header />
 
-        <main className="flex min-h-0 flex-1 flex-col p-4 pt-20 lg:p-6 lg:pt-24">
+        <main className="flex min-h-0 flex-1 flex-col p-4 pt-8 lg:pt-16">
           <div className="flex h-full min-h-0 min-w-0 flex-1 flex-col">
             <Outlet />
           </div>
